dd9ed67e9939d6a9c54fe945103a3206f2658754
patrickodacre <patrick@patrickwho.me>
2016-12-23 16:18:53 -0500

    Add examples of connecting to Airtable. Update pkg info.

--- env.example.js
+++ env.example.js
@@ -1,1 +1,9 @@
 const airtableKey = 'your_key_here'
+
+// Example uses:
+
+// axios.get(`https://api.airtable.com/v0/appgyWdA8yP0KXZr4/My%20Study%20Cards?maxRecords=${records}&view=Main%20View&api_key=${airtableKey}`)
+
+// axios.post(`https://api.airtable.com/v0/appgyWdA8yP0KXZr4/My%20Study%20Cards?api_key=${airtableKey}`, payload)
+
+// axios.delete(`https://api.airtable.com/v0/appgyWdA8yP0KXZr4/My%20Study%20Cards/${studyCardID}?api_key=${airtableKey}`)
